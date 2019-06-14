0121347db6fed4b0f584cd570341a6228921ee31
Thibault <ThibaultFaouz@gmail.com>
2019-06-14 14:32:08 -0400

    more work on server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // requires dependancies
 var express = require("express");
+var bodyParser = require("body-parser");
 
 // sets port to 3000
 var PORT = process.env.PORT || 3000;
@@ -12,6 +13,14 @@ var router = express.Router();
 
 // public folder is static directory
 app.use(express.static(__dirname + "/public"));
+
+
+// enables bodyparser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
 
 // everything goes through router middleware 
 app.use(router);
