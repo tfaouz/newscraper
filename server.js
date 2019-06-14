651603cd8a1348328a0d18612f1f5c6ea6f5cfa6
Thibault <ThibaultFaouz@gmail.com>
2019-06-14 13:50:50 -0400

    server cont

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+// requires dependancies
 var express = require("express");
 
+// sets port to 3000
 var PORT = process.env.PORT || 3000;
+
 
 var app = express();
 
+// sets up express router
 var router = express.Router();
+
+// public folder is static directory
+app.use(express.static(__dirname + "/public"));
+
+// everything goes through router middleware 
+app.use(router);
+
+// listens to the port
+app.listen(PORT, function () {
+    console.log("Listening on port:" + PORT);
+});
