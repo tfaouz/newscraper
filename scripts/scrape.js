2db991d9f84f55fbf84a57c1adbc618ad50b971d
Thibault <ThibaultFaouz@gmail.com>
2019-06-14 17:08:45 -0400

    scrape js comments

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -3,20 +3,27 @@
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+// has call back param
+// requests nyt error response and body
+
 var scrape = function (cb) {
     request("https://www.nytimes.com/", function (err, res, body) {
 
-        var $ = cherio.load(body);
+        var $ = cheerio.load(body);
 
         var articles = [];
 
         $(".theme-summery").each(function (i, element) {
             var head = $(this).children(".story-heading").text().trim();
             var sum = $(this).children(".summary").text().trim();
+            // grabs text cuts off white space at end
+
 
             if (head && sum) {
                 var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
                 var sumNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+                // if head exists and if sum exists
+                // replaces regex method, cleans it up with white space
 
                 var dataToAdd = {
                     headline: headNeat,
@@ -27,7 +34,9 @@ var scrape = function (cb) {
             }
         });
         cb(articles);
-
+        //sends us all the articles
     });
 }
+//exports big scrape variable
+
 module.exports = scrape;
