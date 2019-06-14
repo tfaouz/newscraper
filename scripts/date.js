031b396e3a4069cb631e39ce6a2e678fdfb8ac68
Thibault <ThibaultFaouz@gmail.com>
2019-06-14 17:12:39 -0400

    date comments

--- scripts/date.js
+++ scripts/date.js
@@ -3,11 +3,11 @@ var makeDate = function () {
     var formattedDate = "";
 
     formattedDate += (d.getMonth() + 1) + "_";
-
+    // adds month, +1 because it starts at 0 index
     formattedDate += d.getDate() + "_";
-
+    // adds date
     formattedDate += d.getFullYear();
-
+    //adds year
     return formattedDate;
 };
 
