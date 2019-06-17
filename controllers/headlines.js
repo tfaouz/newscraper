59afcc8c98abf10966e569a0f37c203db4e88d0d
Thibault <ThibaultFaouz@gmail.com>
2019-06-17 11:37:13 -0400

    call back

--- controllers/headlines.js
+++ controllers/headlines.js
@@ -5,5 +5,21 @@ var Headline = require("../models/Headline");
 
 module.exports = {
 
+    fetch: function (cb) {
+
+        scrape(function (data) {
+
+            var articles = data;
+
+            for (var i = 0; i < articles.length; i++) {
+                articles[i].date = makeDate();
+                articles[i].saved = false;
+            }
+
+            Headline.collection.insertMany(articles, { ordered: false }, function (err, docs) {
+                cb(err, docs);
+            });
+        });
+    }
 }
-//16: 16
+//fetch grabs all the articles in the scrape
