4356334e8b6881b23fa188adf973632fe178761d
Thibault <ThibaultFaouz@gmail.com>
2019-06-18 17:51:05 -0400

    trying to scrape lobsters instead

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,45 +1,33 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const instance = axios.create({
-    baseURL: 'https://www.nytimes.com/',
-    timeout: 1000,
-});
+const URL = 'https://lobste.rs';
 
 async function scrape(cb) {
-    console.log('we made it');
-    const items = await instance.request();
-    console.log(items);
-    /*       let $ = cheerio.load(body);
-  
-          let articles = [];
-  
-          $(".theme-summary").each(function (i, element) {
-              let head = $(this).children(".story-heading").text().trim();
-              let sum = $(this).children(".summary").text().trim();
-              // grabs text cuts off white space at end
-              console.log("scrape working")
-  
-  
-              if (head && sum) {
-                  let headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-                  let sumNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-                  // if head exists and if sum exists
-                  // replaces regex method, cleans it up with white space
-  
-                  let dataToAdd = {
-                      headline: headNeat,
-                      summary: sumNeat
-                  };
-                  articles.push(dataToAdd);
-  
-              }
-          });
-          cb(articles);
-          //sends us all the articles
-      }) */
+    const response = await axios.get(URL);
+    // console.log(response.data);
+    const $ = cheerio.load(response.data);
 
+    let articles = [];
+    $('.u_url').each(function (i, element) {
+        /*
+        let link = $(element).find("a").attr("href");
+        let head = $(element).find("h2.headline").text().trim();
+        let sum = $(element).find("p.summary").text().trim();
+        // grabs text cuts off white space at end
+        console.log("scrape working")
+
+        let dataToAdd = {
+            link: link,
+            headline: head,
+            summary: sum
+        };
+
+        console.log('pushing article');
+        
+        articles.push(dataToAdd);
+        */
+    });
 }
-//exports big scrape variable
 
 module.exports = scrape;
